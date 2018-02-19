909ebeaf3ff3460e2dfa6a4454882647816981b2
nsch1 <nick.schooneman1@gmail.com>
2018-02-19 22:41:16 +0100

    Adds shuffleDeck function

--- dealer.js
+++ dealer.js
@@ -6,11 +6,23 @@ const Player = require('./player.js');
 function fillDeck() {
   let suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
 
-  for (var i = 0; i < suits.length; i++) {
-    for (var c = 2; c < 15; c++) {
+  for (let i = 0; i < suits.length; i++) {
+    for (let c = 2; c < 15; c++) {
       this.deck.push(new Card(suits[i], c));
     }
   }
+}
+
+function shuffle(array) {
+  let currentIndex = array.length, temp, rand;
+
+  while(currentIndex) {
+    rand = Math.floor(Math.random() * currentIndex--);
+    temp = array[currentIndex];
+    array[currentIndex] = array[rand];
+    array[rand] = temp;
+  }
+  return array;
 }
 
 class Dealer {
@@ -19,8 +31,16 @@ class Dealer {
     this.current_table;
     fillDeck.bind(this)();
   }
+
+  shuffleDeck() {
+    shuffle(this.deck);
+  }
 }
 
 let test = new Dealer;
+
+console.log(test.deck[0]);
+
+test.shuffleDeck();
 
 console.log(test.deck[0]);
