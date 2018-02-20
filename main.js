5a7e815fd7dee46e96fa26a9301d3c0dca012e9e
nsch1 <nick.schooneman1@gmail.com>
2018-02-20 21:35:18 +0100

    Move tests to main file

---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Table = require('./table.js');
+
+let test = new Table;
+let dealer = test.dealer;
+let player = test.seatPlayer("test");
+
+dealer.currentTable = test;
+
+console.log(player);
+
+dealer.dealCards(2);
+
+console.log(player.hand);
+
+console.log(dealer.deck[dealer.deck.length-1]);
+
+dealer.burnCard();
+
+console.log(dealer.deck[dealer.deck.length-1]);
+
+dealer.playCards();
+
+console.log(test.field);
